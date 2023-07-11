304a292055ac6a84d8bae25986b1ec35c837f182
Nipun Garg <77457267+ngarg2k2@users.noreply.github.com>
2023-07-11 22:54:36 +0530

    Time: 11 ms (27.68%), Space: 13 MB (55.27%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -9,60 +9,42 @@
  */
 class Solution {
 public:
+    void DFS(int src, int prev, unordered_map<int, vector<int> > &mp, int target,vector<int> &ans)
+    {
+        if(target==0)
+        {
+        ans.push_back(src);
+        return;
+        }
+        for(auto x:mp[src])
+        {
+            if(x!=prev)
+            DFS(x,src,mp,target-1,ans);
+        }
+    }
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        vector<int> ans;
-        unordered_map<int, TreeNode*> parent;
+        unordered_map<int,vector<int>> mp;
         queue<TreeNode*> q;
         q.push(root);
-
-        while(!q.empty()){
-            int si = q.size();
-            for(int i = 0; i < si; i++){
-                auto top = q.front();
-                q.pop();
-                
-                if(top -> left){
-                    parent[top->left->val] = top;
-                    q.push(top->left); 
-                }
-
-                if(top -> right){
-                    parent[top->right->val] = top;
-                    q.push(top->right); 
-                }
-            }
-        }
-
-        unordered_map<int, int> visited;
-        q.push(target);
-        while(k-- && !q.empty()){
-            int size = q.size();
-
-            for(int i = 0; i < size; i++){
-                auto top = q.front();
-                q.pop();
-
-                visited[top -> val] = 1;
-
-                if(top -> left && !visited[top->left->val]){
-                    q.push(top -> left);
-                }
-
-                if(top -> right && !visited[top->right->val]){
-                    q.push(top -> right);
-                }
-
-                if(parent[top->val] && !visited[parent[top->val] -> val]){
-                    q.push(parent[top->val]);
-                }
-
-            }
-        }
-
-        while(!q.empty()){
-            ans.push_back(q.front()->val);
+        while(!q.empty())
+        {
+            auto x=q.front();
             q.pop();
+            if(x->left)
+            {
+                q.push(x->left);
+                mp[x->val].push_back(x->left->val);
+                mp[x->left->val].push_back(x->val);
+            }
+            if(x->right)
+            {
+                q.push(x->right);
+                mp[x->val].push_back(x->right->val);
+                mp[x->right->val].push_back(x->val);
+            }
         }
+        vector<int> ans;
+        DFS(target->val,-1,mp,k,ans);
         return ans;
     }
 };
