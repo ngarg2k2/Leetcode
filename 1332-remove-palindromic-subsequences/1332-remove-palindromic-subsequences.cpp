c4c8cc48367d8f90fdc6b717bc88c0dd187b32a0
Nipun Garg <77457267+ngarg2k2@users.noreply.github.com>
2023-07-16 19:39:36 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (20.68%) - LeetHub

---
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        string s1 = s;
+        reverse(s.begin(), s.end());
+        if(s==s1)
+            return 1;
+        return 2;
+    }
+};
