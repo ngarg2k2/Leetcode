292a8444023fb36f51ab13972d7fe27b9c8fd398
Nipun Garg <77457267+ngarg2k2@users.noreply.github.com>
2023-07-22 16:47:41 +0530

    Time: 5 ms (22.83%), Space: 6.5 MB (48.77%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int helper(int i,int j,int m,int n,vector<vector<int>>&vis,int&ans,vector<vector<int>>&dp){
+    int helper(int i,int j,int m,int n,int&ans,vector<vector<int>>&dp){
         if(i<0||i>=m||j<0||j>=n){
             return 0;
         }
@@ -11,18 +11,17 @@ public:
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
-        vis[i][j]=1;
-        int l=helper(i+1,j,m,n,vis,ans,dp);
-        int r=helper(i,j+1,m,n,vis,ans,dp);
-        vis[i][j]=0;
+        
+        int l=helper(i+1,j,m,n,ans,dp);
+        int r=helper(i,j+1,m,n,ans,dp);
         
         return dp[i][j]=l+r;
     }
     int uniquePaths(int m, int n) {
         int ans=0;
-        vector<vector<int>>vis(m,vector<int>(n,0));
+        // vector<vector<int>>vis(m,vector<int>(n,0));
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        return helper(0,0,m,n,vis,ans,dp);
+        return helper(0,0,m,n,ans,dp);
         // return ans;
     }
 };
