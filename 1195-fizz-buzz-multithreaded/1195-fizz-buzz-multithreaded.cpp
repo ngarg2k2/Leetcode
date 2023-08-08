7a2cddaa70b1ff1e54bd7c2488fcf686b56b2a95
Nipun Garg <77457267+ngarg2k2@users.noreply.github.com>
2023-08-08 12:51:30 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (47.62%) - LeetHub

---
+++ 1195-fizz-buzz-multithreaded/1195-fizz-buzz-multithreaded.cpp
@@ -0,0 +1,72 @@
+class FizzBuzz {
+private:
+    int n;
+    condition_variable cv;
+    int i;
+    mutex m;
+public:
+    FizzBuzz(int n) {
+        i=1;
+        this->n = n;
+    }
+
+    // printFizz() outputs "fizz".
+    void fizz(function<void()> printFizz) {
+        while(i<=n){
+            unique_lock<mutex>lock(m);
+            while(i<=n&&(i%3!=0||i%5==0)){
+                cv.wait(lock);
+            }
+            if(i<=n){
+                printFizz();
+                i++;
+            }
+            cv.notify_all();
+        }
+    }
+
+    // printBuzz() outputs "buzz".
+    void buzz(function<void()> printBuzz) {
+        while(i<=n){
+            unique_lock<mutex>lock(m);
+            while(i<=n&&(i%3==0||i%5!=0)){
+                cv.wait(lock);
+            }
+            if(i<=n){
+                printBuzz();
+                i++;
+            }
+            cv.notify_all();
+        }
+    }
+
+    // printFizzBuzz() outputs "fizzbuzz".
+	void fizzbuzz(function<void()> printFizzBuzz) {
+        while(i<=n){
+            unique_lock<mutex>lock(m);
+            while(i<=n&&(i%3!=0||i%5!=0)){
+                cv.wait(lock);
+            }
+            if(i<=n){
+                printFizzBuzz();
+                i++;
+            }
+            cv.notify_all();
+        }
+    }
+
+    // printNumber(x) outputs "x", where x is an integer.
+    void number(function<void(int)> printNumber) {
+        while(i<=n){
+            unique_lock<mutex>lock(m);
+            while(i<=n&&(i%3==0||i%5==0)){
+                cv.wait(lock);
+            }
+            if(i<=n){
+                printNumber(i);
+                i++;
+            }
+            cv.notify_all();
+        }
+    }
+};
